0485a55a829fcb33ec457eae1bec76bd43c99bc0
Thomas <therve38@gmail.com>
2023-04-29 20:36:37 +0200

    fix

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const Config = {
   version: '0.1',
-  apiBaseUrl: 'http://90.66.62.181:3000'
+  apiBaseUrl: 'https://multiplayertournamentonline.fr:3000'
 };
