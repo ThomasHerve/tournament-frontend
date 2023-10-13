ec80656d32a6d01c5197e7cec4ecb4c06ae75e8f
Thomas HERVE <therve38@gmail.com>
2023-10-13 14:27:39 +0200

    centralize url

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,7 @@
+import { api_url } from './environments/environment';
+
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const Config = {
   version: '0.1',
-  apiBaseUrl: 'https://backend.multiplayertournamentonline.fr'
+  apiBaseUrl: api_url
 };
