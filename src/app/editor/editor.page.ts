8e7bacb06fe5f5f4931a10be32ee2677edfac471
Fx73 <fx73000@yahoo.fr>
2023-10-17 23:15:11 +0200

    Save icons

--- src/app/editor/editor.page.ts
+++ src/app/editor/editor.page.ts
@@ -22,14 +22,15 @@ export class EditorPage implements OnInit {
   private renderer: Renderer2;
 
   tournament: TournamentDTO = new TournamentDTO();
-
+  lastcloudsave: Date | null = null
+  lastlocalsave: Date | null = null
 
   constructor(private route: ActivatedRoute, private router: Router, rendererFactory: RendererFactory2, private tournamentService: TournamentService, private editorService: EditorService) {
-    const id = this.route.snapshot.paramMap.get('id')!;
-    if (id == "New") {
+    const id = this.route.snapshot.paramMap.get('id')!.toLocaleLowerCase();
+    if (id == "new") {
       this.tournament = new TournamentDTO()
       this.tournament.creator = UserComponent.user?.username!
-    } else if (id == "Local") {
+    } else if (id == "local") {
       const json = localStorage.getItem('localTournament');
       if (json)
         this.tournament = JSON.parse(json);
@@ -43,6 +44,10 @@ export class EditorPage implements OnInit {
         this.saveLocally()
       }
     });
+
+    setInterval(() => {
+      this.updateElapsedTime();
+    }, 60000);
   }
 
   ngOnInit() {
@@ -51,15 +56,16 @@ export class EditorPage implements OnInit {
   saveLocally() {
     const t = JSON.stringify(this.tournament);
     localStorage.setItem('localTournament', t);
+    this.lastlocalsave = new Date()
   }
 
   saveInCloud() {
     if (this.tournament.id == -1) {
       console.log("trying to add")
-      this.editorService.addTournament(this.tournament).subscribe(() => { localStorage.removeItem('localTournament'); this.router.navigateByUrl('/editor'); });
+      this.editorService.addTournament(this.tournament).subscribe(() => { localStorage.removeItem('localTournament'); this.router.navigateByUrl('/editor'); this.lastcloudsave = new Date(); });
     } else {
       console.log("trying to update " + this.tournament.id)
-      this.editorService.updateTournament(this.tournament).subscribe(() => localStorage.removeItem('localTournament'));
+      this.editorService.updateTournament(this.tournament).subscribe(() => { localStorage.removeItem('localTournament'); this.lastcloudsave = new Date(); });
     }
   }
 
@@ -72,11 +78,40 @@ export class EditorPage implements OnInit {
   }
 
   import() {
-    //let tournament = 
+    //let tournament =
   }
 
   export() {
 
   }
 
+
+  updateElapsedTime() {
+    if (this.lastlocalsave)
+      this.lastlocalsave = new Date(this.lastlocalsave);
+    if (this.lastcloudsave)
+      this.lastcloudsave = new Date(this.lastcloudsave);
+  }
+
+  formatElapsedTime(date: Date | null): string {
+    if (!date) {
+      return 'Never';
+    }
+
+    const now = new Date();
+    const elapsedMilliseconds = now.getTime() - date.getTime();
+    const elapsedSeconds = Math.floor(elapsedMilliseconds / 1000);
+    const elapsedMinutes = Math.floor(elapsedSeconds / 60);
+    const elapsedHours = Math.floor(elapsedMinutes / 60);
+
+    if (elapsedSeconds < 60) {
+      return `just now`;
+    } else if (elapsedMinutes < 60) {
+      return `${elapsedMinutes} min ago`;
+    } else if (elapsedHours < 24) {
+      return `${elapsedHours} hour ago`;
+    } else {
+      return 'Never';
+    }
+  }
 }
